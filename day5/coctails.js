ff04bc7a8d61d95c4a03d8ae49aa8f483bd2efd2
Nazar <rusetskiy2788@mail.ru>
2021-11-16 18:37:38 +0300

    add oop

--- day5/coctails.js
+++ day5/coctails.js
@@ -1,3 +1,8 @@
+let addRecipe = document.querySelector('.write_list__сocktails')
+let removeRecipe = document.querySelector('.delete_recipe')
+let showRecipe = document.querySelector('.read_list_recipe')
+let showAllName = document.querySelector('.all_name__recipe')
+let result = document.querySelector('.result')
 
 class HashStorage {
     constructor() {
@@ -26,11 +31,42 @@ class HashStorage {
   }
   
  
-  const coctail = new HashStorage();
+  const coctailsStorage = new HashStorage();
   
-  coctail.addValue('coffee', {milk: true}); // добавляем значение
-  coctail.addValue('tea', 'tea with lemons'); // добавляем значение
-  console.log(coctail.getValue('coffee'));
-  console.log(coctail.getKeys());
-  console.log(coctail.deleteValue('tea'));
+  coctailsStorage.addValue('coffee', {'alcohol': true}); // добавляем значение
+  coctailsStorage.addValue('tea', 'tea with lemons'); 
+  // добавляем 
+  let addName = ''
+  let addList = {}
+  addRecipe.addEventListener('click' , () => {
+	  addName = prompt('Название рацепта')
+	  addList = prompt('Рецепт')
+	  coctailsStorage.addValue(addName, addList)
+	   
+  })
+
+   showAllName.addEventListener('click' , () => {
+	  
+	  result.textContent = coctailsStorage.getKeys()
+	   
+  })
+
+  
+   showRecipe.addEventListener('click' , () => {
+	  addName = prompt('Название рацепта')
+	  if(coctailsStorage.getValue(addName)){
+		result.textContent = JSON.stringify(coctailsStorage.getValue(addName))
+	  } else result.textContent = 'нет такого рецепта'
+	  
+	   
+  })
+
+  removeRecipe.addEventListener('click' , () => {
+	  addName = prompt('Название рацепта')
+	  result.textContent = JSON.stringify(coctailsStorage.deleteValue(addName))
+	   
+  })
+  
+
+  
   
